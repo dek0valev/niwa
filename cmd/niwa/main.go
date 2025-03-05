494bd3b91a50ef7fb59cc07674e45df369fc14ca
dek0valev <dek0valev@proton.me>
2025-03-05 15:05:06 +0700

    feat: init config in main entrypoint

--- cmd/niwa/main.go
+++ cmd/niwa/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/dek0valev/niwa/internal/config"
+	"github.com/dek0valev/niwa/pkg/must"
+)
 
 func main() {
 	fmt.Println("庭 | Niwa")
+
+	var configPath string
+
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.Parse()
+
+	cfg := must.Must(config.NewConfig(configPath))
+
+	fmt.Println("Env:", cfg.Env)
 }
