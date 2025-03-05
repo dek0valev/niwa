cb6318204c3ec7f4476ae71d8e91a367e15fdaa6
dek0valev <dek0valev@proton.me>
2025-03-05 16:47:46 +0700

    feat: init content store and parser in main entrypoint

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,7 +2,12 @@ package app
 
 import (
 	"github.com/dek0valev/niwa/internal/config"
+	"github.com/dek0valev/niwa/internal/content"
+	"github.com/yuin/goldmark"
+	highlighting "github.com/yuin/goldmark-highlighting/v2"
+	meta "github.com/yuin/goldmark-meta"
 	"log/slog"
+	"os"
 )
 
 type App struct {
@@ -11,6 +16,20 @@ type App struct {
 }
 
 func NewApp(cfg *config.Config, log *slog.Logger) *App {
+	md := goldmark.New(
+		goldmark.WithExtensions(meta.Meta, highlighting.NewHighlighting(
+			highlighting.WithStyle("github-dark"),
+		)),
+	)
+
+	store := content.NewStore()
+
+	parser := content.NewParser(md)
+	if err := parser.ParseDirectory(cfg.Content.ArticlesDir, store); err != nil {
+		log.Error("Не удалось прочитать директорию: " + err.Error())
+		os.Exit(1)
+	}
+
 	return &App{
 		cfg: cfg,
 		log: log,
